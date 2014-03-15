987df7be3936e257f736dd4630c6a0120fc24e26
cameronwp <cameron.w.pittman@gmail.com>
2014-03-14 23:05:45 -0700

    initializing jsfiddle

--- app.js
+++ app.js
@@ -30,6 +30,8 @@ if ( app.get( 'env' ) === 'development' ) {
 }
 
 app.get( '/', routes.index );
+app.get( '/ud_demo', routes.demo );
+app.get( '/jsfiddle', routes.jsfiddle );
 
 http.createServer( app ).listen( app.get( 'port' ), function () {
   console.log( 'Express server listening on port ' + app.get( 'port' ) );
--- routes/index.js
+++ routes/index.js
@@ -14,3 +14,27 @@ exports.index = function(req, res){
     res.end();
     });
 };
+
+exports.demo = function(req, res){
+    fs = require( 'fs' );
+    fs.readFile('./views/demo.html', function (err, html) {
+        if (err) {
+        throw err; 
+    }
+    res.writeHead( 200, { 'content-type': 'text/html' } );
+    res.write( html );
+    res.end();
+    });
+};
+
+exports.jsfiddle = function(req, res){
+    fs = require( 'fs' );
+    fs.readFile('./views/jsfiddle.html', function (err, html) {
+        if (err) {
+        throw err; 
+    }
+    res.writeHead( 200, { 'content-type': 'text/html' } );
+    res.write( html );
+    res.end();
+    });
+};
